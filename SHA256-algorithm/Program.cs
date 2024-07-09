a07fcb064bc71e72d0525e856363a3cf51e85c08
giolga <giolga.kumi@gmail.com>
2024-07-09 14:09:21 +0400

    N Blocks Defined

--- SHA256-algorithm/Program.cs
+++ SHA256-algorithm/Program.cs
@@ -12,6 +12,15 @@ namespace SHA256_algorithm
         private static List<string> messageBits = new List<string>();
         private static List<string> nBlocks = new List<string>();
 
+        private static uint H0 = 0x6a09e667;
+        private static uint H1 = 0xbb67ae85;
+        private static uint H2 = 0x3c6ef372;
+        private static uint H3 = 0xa54ff53a;
+        private static uint H4 = 0x510e527f;
+        private static uint H5 = 0x9b05688c;
+        private static uint H6 = 0x1f83d9ab;
+        private static uint H7 = 0x5be0cd19;
+
         private static string BinaryValue(int asciiValue) // STEP - 1
         {
             string bin = "";
@@ -61,6 +70,19 @@ namespace SHA256_algorithm
             return sixtyFourBits.PadLeft(64, '0');
         }
 
+        private static List<string> DefineBlocks(string paddedMessage) //STEP - 4
+        {
+            List<string> blocks = new List<string>();
+
+            int n = paddedMessage.Length / 512;
+
+            for (int i = 0; i < n; i++)
+            {
+                blocks.Add(paddedMessage.Substring(i * 512, 512));
+            }
+
+            return blocks;
+        }
         static void Main(string[] args)
         {
             //Console.BackgroundColor = ConsoleColor.Black;
@@ -84,8 +106,13 @@ namespace SHA256_algorithm
             //Console.WriteLine($"448 bit value: {MessagePadding(bin)}");
             Console.WriteLine($"n 512 bit value: {MessagePadding(bin) + SixtyFourRepresentation(s.Length * 8)}");
 
+            string paddedMessage = MessagePadding(bin) + SixtyFourRepresentation(s.Length * 8);
+            //Console.WriteLine("Padded Message {0}", paddedMessage);
             Console.WriteLine($"The total length is {MessagePadding(bin).Length + SixtyFourRepresentation(s.Length * 8).Length}");
-            Console.WriteLine($"Number of blocks: {(MessagePadding(bin).Length + SixtyFourRepresentation(s.Length * 8).Length) / 512}");
+            Console.WriteLine($"Number of blocks: {(MessagePadding(bin).Length + SixtyFourRepresentation(s.Length * 8).Length) / 512} \n--------------");
+
+            List<string> blocks = DefineBlocks(paddedMessage);
+            blocks.ForEach(block => Console.WriteLine(block.Length));
 
             Console.ReadKey();
         }
